5ad2d7a1040636401bcf25a4c6a7b423ea114a9c
agilek <michal.acler@gmail.com>
2025-05-02 14:45:53 +0200

    Bug fixes

---
+++ analytics.js
@@ -0,0 +1,8 @@
+// Initialize Umami analytics
+(function() {
+    const script = document.createElement('script');
+    script.src = 'https://cloud.umami.is/script.js';
+    script.setAttribute('data-website-id', '6c900ddc-1afb-4a69-867e-a675c99112b8');
+    script.async = true;
+    document.head.appendChild(script);
+})(); 
